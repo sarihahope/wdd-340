77328636ad3d1c2099c0812e1a481d3c7f6c3abf
sarihahope <sarihaslink@yahoo.com>
2024-10-07 19:31:24 -0700

    Single view

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,7 +1,6 @@
 const invModel = require("../models/inventory-model")
 const utilities = require("../utilities/")
 
-
 const invCont = {}
 
 /* ***************************
@@ -20,20 +19,19 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
-
+// Single view
 invCont.buildSingleVehicle = async function (req, res, next) {
   const inv_id = req.params.invId
-  const data = await invModel.getInventoryById(inv_id)
-  const vehicle = data[0]
+  const data = await invModel.getClassificationById(inv_id)
+  const html = await utilities.buildSingleVehicle(data)
   let nav = await utilities.getNav()
-  res.render("./inventory/detail", {
-    title: vehicle.inv_make + " " + vehicle.inv_model,
+  const vehicle = data
+  res.render("./inventory/singleclassification", {
+    title: vehicle + "TITLE",
     nav,
-    vehicle,
+    html,
   })
 }
-
-
 
 
 module.exports = invCont
--- utilities/index.js
+++ utilities/index.js
@@ -62,23 +62,26 @@ Util.buildClassificationGrid = async function(data){
 
 // Build the spefic vehicle information view HTML
 Util.buildSingleVehicle = async function(data){
-  let vehicle = data[0]
-  let html = '<div id="inv-display">'
-  html += '<h1>' + vehicle.inv_make + ' ' + vehicle.inv_model + '</h1>'
-  html += '<img src="' + vehicle.inv_image + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' on CSE Motors" />'
-  html += '<div id="inv-details">'
+  let html 
+  if (data) {
+  html = '<div id="inv-display">'
+  const vehicle = data
+  html += '<h1>' + vehicle.inv_year + ' ' + vehicle.inv_make + ' ' + vehicle.inv_model + '</h1>'
+  html += '<img src="' + vehicle.inv_image +'" />'
+   html += '<img src="' + vehicle.inv_thumbnail +'" />'
   html += '<h2>Details</h2>'
   html += '<ul>'
-  html += '<li><strong>Price:</strong> $' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</li>'
+  html += '<li><strong>Price:</strong> $' + vehicle.inv_price + '</li>'
   html += '<li><strong>Year:</strong> ' + vehicle.inv_year + '</li>'
   html += '<li><strong>Color:</strong> ' + vehicle.inv_color + '</li>'
   html += '<li><strong>Description:</strong> ' + vehicle.inv_description + '</li>'
   html += '<li><strong>Miles:</strong> ' + vehicle.inv_miles + '</li>'
   html += '</ul>'
   html += '</div>'
-  html += '</div>'
+}
   return html
 }
+
 
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
